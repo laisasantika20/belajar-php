0912a48f6f139ae00f0504c39652faeadabb1995
Laisa Santika <laisasantika86@gmail.com>
2022-10-25 23:43:42 +0700

    belajar php day 1

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "Hello World!<br>";
+
+$nama ="Laisa";
+$umur =21;
+
+echo "Nama Saya <strong>$nama, saya berusia $umur tahun.</br>";
+
+$namaAdik ="Santi";
+$umurAdik ="18";
+
+echo "Nama  adik saya <strong>$namaAdik</strong>, adik saya berusia
+$umurAdik tahun.<br>";
+
+$selisihumur = $umur - $umurAdik;
+echo "Selisih usia saya dan adik saya adalah $selisihumur tahun.";
+
+?>
